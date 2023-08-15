269425186f1ff7abf636b0b0b2f4fda798835d13
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2023-08-16 01:28:32 +0200

    created basic app displaying black screen
    as in https://developer.android.com/develop/ui/views/graphics/opengl/environment#activity

--- MyApplication/app/src/main/java/pl/wsei/mobilne/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/pl/wsei/mobilne/myapplication/MainActivity.java
@@ -2,13 +2,19 @@ package pl.wsei.mobilne.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GLSurfaceView gLView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        gLView = new MyGLSurfaceView(this);
+        setContentView(gLView);
+
     }
 }
---
+++ MyApplication/app/src/main/java/pl/wsei/mobilne/myapplication/MyGLRenderer.java
@@ -0,0 +1,26 @@
+package pl.wsei.mobilne.myapplication;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class MyGLRenderer implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+        //Set the background frame colof
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 unused, int width, int height) {
+        GLES20.glViewport(0,0,width,height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 unused) {
+        //Redraw background color
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+    }
+}
