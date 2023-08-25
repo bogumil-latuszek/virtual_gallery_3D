b0c0f2c870494ea498b034ee5aaed34488937313
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2023-08-25 14:12:17 +0200

    finished lesson define shapes and draw shapes
    first of all all lessons are available on official android website:
     https://developer.android.com/develop/ui/views/graphics/opengl/draw
    
    things to note:
    >you dont need to export classes like you did in javascript, here every class
    can use all other classes under the same framework(it's called package in java)
    >buffers - buffers are places in memory used in data transfer processes between
    two operations with different speeds - for example your computer loads documents
    that you want to print to a buffer, and the printer reads them from there.

--- MyApplication/app/src/main/java/pl/wsei/mobilne/myapplication/MyGLRenderer.java
+++ MyApplication/app/src/main/java/pl/wsei/mobilne/myapplication/MyGLRenderer.java
@@ -7,8 +7,13 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class MyGLRenderer implements GLSurfaceView.Renderer {
+
+    private Triangle triangle;
+
     @Override
     public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+        // initialize a triangle
+        triangle = new Triangle();
         //Set the background frame colof
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     }
@@ -22,5 +27,20 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 unused) {
         //Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        triangle.draw();
     }
+
+    public static int loadShader(int type, String shaderCode){
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
+    }
+
 }
