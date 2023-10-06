6e5fb34c56ff6ffa47e4b56005b5bf18603b3cc4
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2023-10-06 12:48:02 +0200

    Removed title bar and locked screen orientation to landscape for Mode3DActivity

--- MyApplication/app/src/main/java/pl/wsei/mobilne/myapplication/Mode3DActivity.java
+++ MyApplication/app/src/main/java/pl/wsei/mobilne/myapplication/Mode3DActivity.java
@@ -2,6 +2,7 @@ package pl.wsei.mobilne.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
@@ -10,6 +11,7 @@ public class Mode3DActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         gLView = new MyGLSurfaceView(this);
         setContentView(gLView);
     }
