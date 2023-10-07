63fada92710152bcb1ecf7a7f9772b9b0c8da2e5
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2023-10-07 04:15:37 +0200

    added grid

--- MyApplication/app/src/main/java/pl/wsei/mobilne/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/pl/wsei/mobilne/myapplication/MainActivity.java
@@ -7,8 +7,12 @@ import android.content.pm.ActivityInfo;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,34 @@ public class MainActivity extends AppCompatActivity {
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+
+
+        String[] flowerName = {"Rose","Lotus","Lily","Jasmine",
+                "Tulip"};
+        int[] flowerImages = {R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background};
+
+        GridAdapter gridAdapter = new GridAdapter(MainActivity.this,flowerName,flowerImages);
+        GridView gridView = findViewById(R.id.gridview);
+        gridView.setAdapter(gridAdapter);
+
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(MainActivity.this, "You Clicked on " + flowerName[position], Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+
+
     }
 
     public void ChangeModeTo3D(View v){
