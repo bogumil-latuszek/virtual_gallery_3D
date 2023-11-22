0097a670c05dd4db8aac02062aaee689c9f2b843
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2023-11-22 17:50:08 +0100

    drawing more than 1 triangle
    also drawing of a cube in 1 command

--- MyApplication/app/src/main/java/pl/wsei/mobilne/myapplication/Cuboid.java
+++ MyApplication/app/src/main/java/pl/wsei/mobilne/myapplication/Cuboid.java
@@ -5,6 +5,7 @@ import android.opengl.GLES20;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 
 public class Cuboid {
 
@@ -29,18 +30,54 @@ public class Cuboid {
                     "}";
     private final int mProgram;
     private FloatBuffer vertexBuffer;
+    private ShortBuffer drawInstructionBuffer;
+    
+    //*************************************************************
+    //EXPERIMENT
+    private ShortBuffer drawInstruction1Buffer;
+    private ShortBuffer drawInstruction2Buffer;
+    //*************************************************************
 
     // number of coordinates per vertex in this array
     static final int COORDS_PER_VERTEX = 3;
     static float[] cuboidCoords = {   // in counterclockwise order:
-            0.0f,  0.622008459f, -0.01f, // top
-            -0.5f, -0.311004243f, 0.1f, // bottom left
-            0.5f, -0.311004243f, -0.01f  // bottom right
+            0.0f,  0.622008459f, 0.0f, // top
+            -0.5f, -0.311004243f, 0.0f, // bottom left
+            0.5f, -0.311004243f, 0.0f,  // bottom right
+            0.5f, 0.622008459f, 0.0f, // top right
+            -0.5f, 0.622008459f, 0.0f // top left
     };
+
+    static short[] instruction = {
+            0, 1, 2, 0, 2, 3, 4, 1, 0
+    };
+
+    //*************************************************************
+    //EXPERIMENT
+    static short[] instruction1 = {//tego nie widzi?
+            4,1,3
+    };
+    static short[] instruction2 = {
+            1,2,3
+    };
+    //*************************************************************
+
+    /*
+    static float[] CuboidCoordsINSTRUCTED = {   // in counterclockwise order:
+            0.0f,  0.622008459f, -0.01f, // top
+            -0.5f, -0.311004243f, -0.01f, // bottom left
+            0.5f, -0.311004243f, -0.01f,  // bottom right
+            0.0f,  0.622008459f, -0.01f, // top
+            0.5f, -0.311004243f, -0.01f,  // bottom right
+            0.5f, 0.622008459f, -0.01f // top right
+    };*/
 
     // Set color with red, green, blue and alpha (opacity) values
     float color[] = { 0.63671875f, 0.76953125f, 0.22265625f, 1.0f };
-
+    //*************************************************************
+    //EXPERIMENT
+    float color2[] = { 0.9f, 0.1f, 0.1f, 1.0f };
+    //*************************************************************
 
     private int positionHandle;
     private int colorHandle;
@@ -71,6 +108,35 @@ public class Cuboid {
         // creates OpenGL ES program executables
         GLES20.glLinkProgram(mProgram);
 
+        //initialize byte buffer for instruction
+        ByteBuffer instructionBuffer = ByteBuffer.allocateDirect(
+                instruction.length * 2);
+        instructionBuffer.order(ByteOrder.nativeOrder());
+
+
+        drawInstructionBuffer = instructionBuffer.asShortBuffer();
+        drawInstructionBuffer.put(instruction);
+        drawInstructionBuffer.position(0);
+
+
+        //*************************************************************
+        //EXPERIMENT
+        //initialize byte buffer for instruction1
+        ByteBuffer instruction1Buffer = ByteBuffer.allocateDirect(
+                instruction1.length * 2);
+        instruction1Buffer.order(ByteOrder.nativeOrder());
+        drawInstruction1Buffer = instruction1Buffer.asShortBuffer();
+        drawInstruction1Buffer.put(instruction1);
+        drawInstruction1Buffer.position(0);
+
+        //initialize byte buffer for instruction2
+        ByteBuffer instruction2Buffer = ByteBuffer.allocateDirect(
+                instruction2.length * 2);
+        instruction2Buffer.order(ByteOrder.nativeOrder());
+        drawInstruction2Buffer = instruction2Buffer.asShortBuffer();
+        drawInstruction2Buffer.put(instruction2);
+        drawInstruction2Buffer.position(0);
+        //*************************************************************
 
         // initialize vertex byte buffer for shape coordinates
         ByteBuffer bb = ByteBuffer.allocateDirect(
@@ -115,7 +181,23 @@ public class Cuboid {
         GLES20.glUniformMatrix4fv(vPMatrixHandle, 1, false, mvpMatrix, 0);
 
         // Draw the cuboid
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+        //GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount); //mode, first, count
+        
+        /*GLES20.glDrawElements(
+                GLES20.GL_TRIANGLES, instruction.length,
+                GLES20.GL_UNSIGNED_SHORT, drawInstructionBuffer);*/
+        //*****************************************************
+        //EXPERIMENT
+        GLES20.glDrawElements(
+                GLES20.GL_TRIANGLES, instruction1.length,
+                GLES20.GL_UNSIGNED_SHORT, drawInstruction1Buffer);
+
+        GLES20.glUniform4fv(colorHandle, 1, color2, 0);
+
+        GLES20.glDrawElements(
+                GLES20.GL_TRIANGLES, instruction2.length,
+                GLES20.GL_UNSIGNED_SHORT, drawInstruction2Buffer);
+        //*****************************************************
 
         // Disable vertex array
         GLES20.glDisableVertexAttribArray(positionHandle);
