48d64252ee8a2f9ebc9bd392fbc694d35131501d
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2023-12-16 17:19:54 +0100

    clean up comments in Cuboid2

--- app/src/main/java/pl/wsei/mobilne/myapplication/Cuboid2.java
+++ app/src/main/java/pl/wsei/mobilne/myapplication/Cuboid2.java
@@ -7,7 +7,7 @@ import java.nio.ByteBuffer;
 
 public class Cuboid2 {
     // number of coordinates per vertex in this array
-    private static final int COORDS_PER_VERTEX = 3;//why call it coords? Can't we name them variables_per_vertex?
+    private static final int COORDS_PER_VERTEX = 3;
     //not sure what this code below was meant for
     /*
     private static final int CUBE_LINES_NB = 12;
@@ -16,16 +16,15 @@ public class Cuboid2 {
     */
     private final float[] modelMatrix = new float[16]; //a 4x4 matrix
 
-    private float[] edgeColor = {37f/256f, 58f/256f, 190f/256f}; // default is blue // why do we use floats for color data?
+    private float[] edgeColor = {37f/256f, 58f/256f, 190f/256f}; // opengl requires color as float in range 0-1
     private float[] faceColor = {81f/256f, 97f/256f, 203f/256f}; // default is blue
     private float faceOpacity = 0.5f; // default is 50%
-    //private static final int BYTES_PER_FLOAT = 4; unused?
     private final VertexArray vertexArray;  // <-- Vertices
-    private final ByteBuffer instructionForDrawingFaces;
-    private final ByteBuffer instructionForDrawingEdges;
+    private final ByteBuffer vertexSequenceForDrawingFaces;
+    private final ByteBuffer vertexSequenceForDrawingEdges;
 
 
-    public Cuboid2(float dx, float dy, float dz) { //constructing cuboid based on width, heigth, length
+    public Cuboid2(float dx, float dy, float dz) {
         // prepare buffer for vertices
         vertexArray = new VertexArray(new float[]{
                 -dx, dy, dz,             // (0) Top-left near
@@ -37,7 +36,7 @@ public class Cuboid2 {
                 -dx, -dy, -dz,           // (6) Bottom-left far
                 dx, -dy, -dz             // (7) Bottom-right far
         });
-        instructionForDrawingFaces = ByteBuffer.allocateDirect(3 * 2 * 6)//num. of vertexes per triange * triangles per face * faces per cuboid
+        vertexSequenceForDrawingFaces = ByteBuffer.allocateDirect(3 * 2 * 6)//num. of vertexes per triange * triangles per face * faces per cuboid
                 .put(new byte[]{
                         // Front - counter-clockwise (front-facing)
                         1, 0, 3,
@@ -58,8 +57,8 @@ public class Cuboid2 {
                         7, 3, 6,
                         6, 3, 2
                 });
-        instructionForDrawingFaces.position(0); //set starting position
-        instructionForDrawingEdges = ByteBuffer.allocateDirect(3 * 2 * 6)
+        vertexSequenceForDrawingFaces.position(0); //set starting position
+        vertexSequenceForDrawingEdges = ByteBuffer.allocateDirect(3 * 2 * 6)
                 .put(new byte[]{
                         // vertical Y-axis edges
                         0, 2,
@@ -77,7 +76,7 @@ public class Cuboid2 {
                         3, 7,
                         2, 6,
                 });
-        instructionForDrawingEdges.position(0);
+        vertexSequenceForDrawingEdges.position(0);
     }
     public void setEdgeColor(float[] color) {
         //  float with 3 values:   R   G   B
@@ -105,19 +104,7 @@ public class Cuboid2 {
         Matrix.scaleM(modelMatrix, 0, x, y, z);
     }
 
-    //nałóż otrzymane modelViewProjectionMatrix i viewProjectionMatrix na naszą wewnętrzną modelMatrix
-    //jeśli dobrze to rozumiem to model matrix NIE JEST array-em vertexów, a właściwie jedynie określa
-    //punkt origin całej bryły, tzn dla wszystkich punktów centrum jest w punkcie 0,0,0 dla macierzy
-    //identity, czyli w większości przypadków defaultowa macierz to macierz identity, a jeśli chcemy
-    //gdzieś przemieścić dany obiekt to mimo wszystko zawsze spawnujemy go w (0,0,0) i dopiero nakładamy na niego
-    // transformacje, czyli każdy obiekt jest "czysty" w sensie że po stworzeniu spawni się w (0,0,0), chyba żę
-    // dajmy na to mamy masę obiektów w scenie i nie chce nam się za każdym razem mnożyć macierzy pozycji w świecie
-    // razy macierz identity, wtedy możemy zrobić overload konstruktora żeby brał gotową macierz modelMatrix jako parametr.
-    // Ale przecierz jeśli ta operacja jest wykonywana tylko w momencie loadowania sceny, to może nie mieć to sensu
-    public void combineWithModelMatrix(float[] viewProjectionMatrix, float[] modelViewProjectionMatrix) {
-        Matrix.multiplyMM(modelViewProjectionMatrix, 0, viewProjectionMatrix, 0, modelMatrix, 0); //what happens if we change the order
-    }
-    public void draw(int aPositionLocation, int uColorLocation, int useGlobalColorLocation, //overload a function?
+    public void draw(int aPositionLocation, int uColorLocation, int useGlobalColorLocation, //overloading draw  function
                      int uMatrixLocation, float[] viewProjectionMatrix) {
         draw(aPositionLocation, uColorLocation, useGlobalColorLocation,
                 uMatrixLocation, viewProjectionMatrix, this.edgeColor, this.faceColor);
@@ -126,16 +113,17 @@ public class Cuboid2 {
                      int uMatrixLocation, float[] viewProjectionMatrix,
                      float[] edgeColor, float[] faceColor) {
 
-        // force shader to use uniform color
+
+        // force shader to use uniform color // todo: clarify naming
         int trueInGPU = 1;
-        GLES20.glUniform1i(useGlobalColorLocation, trueInGPU); // what is useGlobalColorLocation? A switch?
+        GLES20.glUniform1i(useGlobalColorLocation, trueInGPU);
 
         // prepare vertices buffer (floats --> bytes)
         prepareDataSource_forPositionAttribute(aPositionLocation);
 
         // recalculate vertices per matrices
         float[] modelViewProjectionMatrix = new float[16];
-        combineWithModelMatrix(viewProjectionMatrix, modelViewProjectionMatrix);
+        Matrix.multiplyMM(modelViewProjectionMatrix, 0, viewProjectionMatrix, 0, modelMatrix, 0); //what happens if we change the order?
         GLES20.glUniformMatrix4fv(uMatrixLocation, 1, false, modelViewProjectionMatrix, 0);
 
         // Opacity must be enabled and opacity-mode selected
@@ -147,13 +135,13 @@ public class Cuboid2 {
         GLES20.glUniform4f(uColorLocation, edgeColor[0], edgeColor[1], edgeColor[2], 1.0f);
 
         int nbIndexes4lines = 2*4*3;
-        GLES20.glDrawElements(GLES20.GL_LINES, nbIndexes4lines, GLES20.GL_UNSIGNED_BYTE, instructionForDrawingEdges);
+        GLES20.glDrawElements(GLES20.GL_LINES, nbIndexes4lines, GLES20.GL_UNSIGNED_BYTE, vertexSequenceForDrawingEdges);
 
         // another words: set color for drawing faces
         GLES20.glUniform4f(uColorLocation, faceColor[0], faceColor[1], faceColor[2], this.faceOpacity);
 
         int nbIndexes4triangles = 2*3*6;
-        GLES20.glDrawElements(GLES20.GL_TRIANGLES, nbIndexes4triangles, GLES20.GL_UNSIGNED_BYTE, instructionForDrawingFaces);
+        GLES20.glDrawElements(GLES20.GL_TRIANGLES, nbIndexes4triangles, GLES20.GL_UNSIGNED_BYTE, vertexSequenceForDrawingFaces);
 
         GLES20.glDisable(GLES20.GL_BLEND);
     }
