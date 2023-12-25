11b7ce692e189e8da09552a27553c5cba43f2349
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2023-12-25 19:26:48 +0100

    Wall data model for db

---
+++ app/src/main/java/pl/wsei/mobilne/myapplication/database/Wall.java
@@ -0,0 +1,15 @@
+package pl.wsei.mobilne.myapplication.database;
+
+public class Wall {
+
+    private float x;
+    private float y;
+
+    public float getX() {return x;}
+    public float getY() {return y;}
+
+    public Wall(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
