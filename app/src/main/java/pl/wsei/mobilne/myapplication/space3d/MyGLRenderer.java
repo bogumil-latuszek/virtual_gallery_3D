ceb24e4d8379870e587e9fa7b80a64671cd56638
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2024-02-07 17:18:18 +0100

    added correction of movement by rotating movement vector with ViewProjectionMatrix

--- app/src/main/java/pl/wsei/mobilne/myapplication/space3d/MyGLRenderer.java
+++ app/src/main/java/pl/wsei/mobilne/myapplication/space3d/MyGLRenderer.java
@@ -378,8 +378,12 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         this.movementCtrl.updatePointPressed(pointPressed);
         Vector3D moveVector3D = this.movementCtrl.getMovementVector();
         //this.projectionMatrix.
-        Matrix.translateM(viewMatrix,0, moveVector3D.x, moveVector3D.y, moveVector3D.z);
-
+        float[] moveVector4D = new float[]{moveVector3D.x, moveVector3D.y, moveVector3D.z, 0};
+        float[] targetVector4D = new  float[4];
+        Matrix.multiplyMV(targetVector4D, 0, viewProjectionMatrix, 0, moveVector4D, 0 );
+        targetVector4D[1] = 0; // y coord must remain 0  so we won't fly
+        //Matrix.translateM(viewMatrix,0, moveVector3D.x, moveVector3D.y, moveVector3D.z);
+        Matrix.translateM(viewMatrix,0, targetVector4D[0], targetVector4D[1], targetVector4D[2]);
 
 
 
