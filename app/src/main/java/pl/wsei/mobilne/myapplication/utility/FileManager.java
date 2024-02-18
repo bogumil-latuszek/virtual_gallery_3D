50bb4e437e43189cf4ce251e4fe123c21cc54a2a
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2024-02-18 22:28:24 +0100

    clean FileManager.java from unused code

--- app/src/main/java/pl/wsei/mobilne/myapplication/utility/FileManager.java
+++ app/src/main/java/pl/wsei/mobilne/myapplication/utility/FileManager.java
@@ -40,9 +40,6 @@ public class FileManager {
             e.printStackTrace();
             return  null;
         }
-
-        //path = Environment.getExternalStorageDirectory().toString()+"/Pictures";
-
     }
 
     public static void  listFiles(){
@@ -113,27 +110,5 @@ public class FileManager {
                 e.printStackTrace();
             }
         }
-
-//        ContextWrapper cw = new ContextWrapper(context);
-//        // path to /data/data/yourapp/app_data/imageDir
-//        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
-//        // Create imageDir
-//        File mypath=new File(directory,"profile.jpg");
-//
-//        FileOutputStream fos = null;
-//        try {
-//            fos = new FileOutputStream(mypath);
-//            // Use the compress method on the BitMap object to write image to the OutputStream
-//            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                fos.close();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        return directory.getAbsolutePath();
     }
 }
